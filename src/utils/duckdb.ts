a116568bec33c952ecc97691c67a748f7a20b2c1
Vedanta Somnathe <vedanta1412@gmail.com>
2025-11-17 05:39:29 -0600

    duck in md

--- src/utils/duckdb.ts
+++ src/utils/duckdb.ts
@@ -16,7 +16,6 @@ export const getDuckDB = createServerOnlyFn(async () => {
   if (!duckDBInstance) {
     duckDBInstance = await DuckDBInstance.create(
       `md:?token=${process.env.MD_ACCESS_TOKEN}`,
-      { home_directory: '/tmp' },
     )
   }
   return duckDBInstance
