1b5d4572813a335854174f744df171307e13dec5
Vedanta Somnathe <vedanta1412@gmail.com>
2025-11-17 03:22:01 -0600

    duck in temp

--- src/utils/duckdb.ts
+++ src/utils/duckdb.ts
@@ -14,7 +14,7 @@ let duckDBInstance: DuckDBInstance | null = null
 
 export const getDuckDB = createServerOnlyFn(async () => {
   if (!duckDBInstance) {
-    duckDBInstance = await DuckDBInstance.create('./tmp/myduck.db')
+    duckDBInstance = await DuckDBInstance.create('/tmp/myduck.db')
   }
   return duckDBInstance
 })
